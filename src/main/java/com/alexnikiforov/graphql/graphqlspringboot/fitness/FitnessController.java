b9e69d6c4ee8e2290ab905ef0753cc2350fccf52
alexander.nikiforov <nikiforov.san.sanich@gmail.com>
2023-09-15 16:42:30 +0400

    02. Difficulty is fetched by separate thread pool

--- src/main/java/com/alexnikiforov/graphql/graphqlspringboot/fitness/FitnessController.java
+++ src/main/java/com/alexnikiforov/graphql/graphqlspringboot/fitness/FitnessController.java
@@ -6,12 +6,17 @@ import com.alexnikiforov.graphql.graphqlspringboot.fitness.domain.FitnessClass;
 import graphql.GraphQLContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -29,8 +34,21 @@ public class FitnessController {
                 .coach(coach)
                 .startsAt(startsAt)
                 .endsAt(startsAt.plusHours(1L))
-                .difficulty(Difficulty.BEGINNER)
                 .build()
         );
+    }
+
+    //    We assume that we need to call an external service to get this Difficulty information
+    @BatchMapping
+    public Callable<Map<FitnessClass, Difficulty>> difficulty(
+            List<FitnessClass> fitnessClasses,
+            GraphQLContext context
+    ) {
+        log.info("difficulty({})", fitnessClasses);
+        return () -> fitnessClasses.stream()
+                        .collect(Collectors.toUnmodifiableMap(
+                                Function.identity(),
+                                ignore -> Difficulty.BEGINNER
+                        ));
     }
 }
